2b27fa94319d53f7ca0aaf2f345387e30bc29cd8
Alejandra-66 <ludmolina@uniboyaca.edu.co>
2025-03-22 09:56:29 -0500

    Alejandra: cambios en index.php

--- index.php
+++ index.php
@@ -9,12 +9,20 @@
 </head>
 <body>
     <table>
-        
+
     </table>
     <h1>Ejercio 1</h1>
     <a href="ejercicio1.php"> Ejercicio 1</a>
     <h1>Ejercicio 2</h1>
-    <a href="exercise2">Ejercico 2</a>
+    <a href="exercise2.php">Ejercico 2</a>
+    <h1>Ejercicio 3</h1>
     <a href="">Ejercicio 3</a>
+    <h1>Ejercio 4</h1>
+    <a href=""> Ejercicio 4</a>
+    <h1>Ejercio 5</h1>
+    <a href=""> Ejercicio 5</a>
+    <h1>Ejercio 6</h1>
+    <a href=""> Ejercicio 6</a>
+    
 </body>
 </html>
