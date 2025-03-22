a20ded56bdab8699918ba3a60c11c93fc24517cd
unknown <dsgil@uniboyaca.edu.co>
2025-03-22 09:48:47 -0500

    ejercicio 1

--- metodos.php
+++ metodos.php
@@ -3,23 +3,27 @@ class Metodos {
 
     function __construct(){
     }
-
     public function ejercicio1($palabra) {
         if (empty($palabra)) {
             return "No ingresaste ninguna palabra.";
+        }
+
+        if (is_numeric($palabra)) {
+            return "Error: Se ingresó un número. Por favor, ingresa un texto válido.";
         }
 
         $separarPalabra = preg_split('/[\s-]+/', $palabra);
 
         $acronimo = '';
         foreach ($separarPalabra as $palabraIndividual) {
-            $acronimo .= strtoupper($palabraIndividual[0]);
+            if (!empty($palabraIndividual)) {
+                $acronimo .= strtoupper($palabraIndividual[0]);
+            }
         }
 
-        $mensaje = "<p>Palabra ingresada: " . htmlspecialchars($palabra) . "</p>";
-        $mensaje .= "<p>Acrónimo generado: " . htmlspecialchars($acronimo) . "</p>";
+        $mensaje = "<p>Palabra ingresada: " . $palabra . "</p>";
+        $mensaje .= "<p>Acrónimo generado: " . $acronimo. "</p>";
         return $mensaje;
     }
-
 }
-
+?>
