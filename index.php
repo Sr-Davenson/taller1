1f82ea343e93390256c6c84004798053e3cbe644
licia2374 <vlrios@uniboyaca.edu.com>
2025-03-24 18:43:27 -0500

    valerin, arreglos en ejercicio 4

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
     <h1>Ejercicio 2</h1>
     <a href="exercise2.php">Ejercico 2</a>
     <h1>Ejercicio 3</h1>
-    <a href="">Ejercicio 3</a>
+    <a href="Ejercicio3.php">Ejercicio 3</a>
     <h1>Ejercio 4</h1>
-    <a href=""> Ejercicio 4</a>
+    <a href="Ejercicio4.php"> Ejercicio 4</a>
     <h1>Ejercio 5</h1>
     <a href=""> Ejercicio 5</a>
     <h1>Ejercio 6</h1>
