18272bbe3dac0a2c1b089cebe951776e3e7f5238
unknown <dsgil@uniboyaca.edu.co>
2025-03-24 19:31:50 -0500

    se completaron los estilo en ejercicio 1

--- ejercicio1Resultado.php
+++ ejercicio1Resultado.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Resultado Ejercicio 1</title>
+    <link rel="stylesheet" href="styles1.css">
 </head>
 <body>
+    <h1>Resultado Ejercicio!</h1>
+    <div class="info">
 <?php
     include "metodos.php";
 
@@ -14,7 +17,11 @@
 
     echo $metodos->ejercicio1($palabra);
 ?>
+    </div>
+    <div class="bot">
 <a href="ejercicio1.php">Convertir otra palabra</a> <br>
+</div>
+<br>
 <a href="index.php">Ir a inicio</a>
 </body>
 </html>
