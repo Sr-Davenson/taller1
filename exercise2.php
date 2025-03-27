db3ffecd8878d506cc993fe53a71b0d25dd6272c
unknown <dsgil@uniboyaca.edu.co>
2025-03-26 22:20:49 -0500

    se ajustaron estilos en ejercicio 2

--- exercise2.php
+++ exercise2.php
@@ -31,21 +31,27 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Ejercicio 2</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="s.css">
 </head>
 <body>
     <h1>Calculadora de Fibonacci y Factorial</h1>
-
+    <div class="info">
+        <br>
     <form method="POST" action="">
         <label for="numero">Ingrese un número:</label>
-        <input type="number" id="numero" name="numero" required><br><br>
-
-        <label for="operacion">Seleccione la operación:</label><br>
-        <input type="radio" id="fibonacci" name="operacion" value="fibonacci" required>
-        <label for="fibonacci">Fibonacci</label><br>
-        <input type="radio" id="factorial" name="operacion" value="factorial" required>
-        <label for="factorial">Factorial</label><br><br>
+        <input type="number" id="numero" name="numero" required>
         <button type="submit">Calcular</button>
+        <br><br>
+        <label for="operacion">Seleccione la operación:</label><br>
+        <div class="opc">
+        <br>
+            <label for="fibonacci">Fibonacci</label>
+            <input type="radio" id="fibonacci" name="operacion" value="fibonacci" required>
+            <br> 
+            <label for="factorial">Factorial</label>  
+            <input type="radio" id="factorial" name="operacion" value="factorial" required>
+
+        </div> 
     </form>
 
     <!--Mensajes de error o resultados -->
@@ -57,6 +63,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <h2>Resultado:</h2>
         <p><?php echo htmlspecialchars($resultado); ?></p>
     <?php endif; ?>
-    <a href="index.php">Volver</a>
+    </div>
+    <br>
+    <a href="index.php">Ir a inicio</a>
+</body>
 </body>
 </html>
--- index.php
+++ index.php
@@ -14,27 +14,27 @@
             <td></td>
         </tr>
         <tr>
-            <td><h2>1.Convertidor de nombres a acrónimos</h2></td>
+            <td><h2>1. Convertidor de nombres a acrónimos</h2></td>
             <td><a href="ejercicio1.php">Ir al ejercicio</a></td>
         </tr>
         <tr>
-            <td><h2>2.Calculadora de fibonacci y factorial</h2></td>
+            <td><h2>2. Calculadora de fibonacci y factorial</h2></td>
             <td><a href="exercise2.php">Ir al ejercicio</a></td>
         </tr>
         <tr>
-            <td><h2>3.Cálculadora estadística</h2></td>
+            <td><h2>3. Cálculadora estadística</h2></td>
             <td><a href="Ejercicio3.php">Ir al ejercicio</a></td>
         </tr>
         <tr>
-            <td><h2>4.Calculadora de conjuntos</h2></td>
+            <td><h2>4. Calculadora de conjuntos</h2></td>
             <td><a href="Ejercicio4.php">Ir al ejercicio</a></td>
         </tr>
         <tr>
-            <td><h2>5.Convertidor de números a binarios</h2></td>
+            <td><h2>5. Convertidor de números a binarios</h2></td>
             <td><a href="ejercicio5.php">Ir al ejercicio</a></td>
         </tr>
         <tr>
-            <td><h2>6.Recorridos Árbol Binario</h2></td>
+            <td><h2>6. Recorridos Árbol Binario</h2></td>
             <td><a href="exercise6.php">Ir al ejercicio</a></td>
         </tr>
     </table> 
